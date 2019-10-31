b71b808adef951ad69ec093fc26dff8887f5f1e3
sai <saisubramani68@gmail.com>
2019-10-31 20:36:27 +0530

    pyhon script completed

--- python_script_read.py
+++ python_script_read.py
@@ -11,22 +11,28 @@ def main():
 		print("File path does not exist... !")
 		sys.exit()
 
+	line_count = 0
+
 	with open(filepath) as fp:
-		#count = 0
+		
 		for line in fp:
+			line_count += 1
 			if ("thanks" in line.lower()):
 				print("End of File...")
 				sys.exit()
-
+			
 			if ("buyer" in line.lower()):
+				print("--------buyer----------")
 				Buyer = line.partition(':')
 				print("Buyer:",Buyer[2])
 			
 			if ("cargo" in line.lower()):
+				print("--------cargo----------")
 				Cargo = line.partition(':')
 				print("Cargo:",Cargo[2]) 
-
+			
 			if ("quantity" in line.lower()):
+				print("--------quantity----------")
 				Quantity = line.partition(':')
 				data = Quantity[2].lower()
 				d=data.replace("'","")
@@ -39,8 +45,9 @@ def main():
 					print('Option : seller') if "sellers" in s else print('Option : buyers')
 					Tolerance = s[4]
 					print("Tolerance:",Tolerance)
-
+			
 			if ('terms' in line.lower()):
+				print("--------terms----------")
 				method = line.partition(':')
 				terms_data = method[2].split(" ")
 				terms_data1 =terms_data[1].lower()
@@ -50,11 +57,12 @@ def main():
 
 			
 			if ('delivery' in line.lower()) and count == 0:
+				print("--------delivery----------")
 				count += 1
 				deleivery = line.partition(':')
 				
 				date_data = deleivery[2].split("to")
-				print(date_data)
+				#print(date_data)
 				laycan_from = date_data[1].replace(',',"")
 				laycan_to = date_data[0].replace(',',"")
 
@@ -71,20 +79,136 @@ def main():
 				print("narr_down:",narr_down)
 
 
+			if ('price' in line.lower()):
+				print("--------price---------")
+				pri = line.partition(":")
+				pric = (pri[2].lower()).replace(" ","")
+				if('mt' in pric):
+					print("price unit field:mt")
+				elif('bbl' in pric):
+					print("price unit field:bbl")
+				if('bl' in pric):
+					print("invoice:BL")
+				elif('out-turn' in pric):
+					print("invoice:out-turn")
+				else:
+					print("invoice:BL")
+				if('platts' in pric):
+					print("Pricing basic:platts")
+				elif('agurus' in pric):
+					print("Pricing basic :agurus")
+				elif('futures' in pric):
+					print("Pricing basic : futures")
+				else:
+					print("Pricing basic:platts")
+
+				discount = re.findall('\d+',pric)[0]
+
+				if('plus' or '+' in pric):
+					a="+"+str(discount)
+					print("premium/discount :",a)
+
+				elif('minus' or "-" in pric):
+					a="-"+str(discount)
+					print("premium/discount :",a)
+
+				if('mean' in pric):
+					tup = (pri[2].lower()).split(" ")
+					inde = tup.index('mean')
+					print("index:",inde)
+					print("tup:",tup)
+					for i in range(inde,0,-1):
+						if(i == 4):
+							check = str(tup[i-1])+" "+str(tup[inde])
+							print(check)
+							print("check with db if not break")
+					for i in range(inde,0,-1):
+						if (i == 4):
+							check = str(tup[i-2])+" "+str(tup[i-1]+" "+str(tup[inde]))
+							print(check)
+							print("check with db")
+						
+			
 			if ('pricing' in line.lower()):
+				print("--------pricing----------")
 				pricing_list=['after bl','around bl','before bl','after nor','around nor','before nor']
 				price = str(line.lower())
 				price_sp = price.partition(':')[2]
-				print(price_sp.find('bl'))
+				string1 = (price_sp).replace(" ","")
+				print('bl') if('bl' in string1) else('nor')
+				if('after' in string1):
+					print("after")
+				elif('before' in string1):
+					print("before")
+				else:
+					print("around")
 				numbers = re.findall('\d+',price)
 				price_list = price.split(' ')
-				print(price_list)
+				#print(price_list)
 				print(numbers[0])
+
+			
+			if ('payment' in line.lower()):
+				print("--------payment----------")
+				print("line_count:",line_count)
+				string2 = str(line.lower()).replace(" ","")
+				if("standbylc" in string2):
+					print("standby LC")
+				elif( "documentary LC" in string2 ):
+					print(" documentary LC")
+				elif("open account" in string2):
+					print("open account")
+				n = string2.find("at")+2
+				m=n
+				#print("n:",n)
+				for n in range(len(string2)):
+					if string2[n].isdigit():
+						m += 1
+						break
+				#print("m:",string2)
+				credit = string2[n:m+1]
+				print("credit:",credit)
+
+				if ('bl' in string2):
+					n = string2.find("bl")
+					print(string2[n:n+2])
+				else:
+					n = string2.find("nor")
+					print(string2[n:n+3])
+
+				num = re.findall('\d+',string2)[1]
+				string3 = "lctobeissued"
+				str3_len =len(string3)
+				issue = string2.find("lctobeissued")+str3_len
+				m = issue+len(num)
+				issued = string2[issue:m]
+				print("issued:",issued)
+
+				if("loading" in string2):
+					print("loading")
+				elif("delivery" in string2):
+					print("delivery")
+				elif("discharge" in string2):
+					print("discharge")
+				
+
+			if ('delivery' in line.lower()):
+				string = str(line.lower()).replace(" ","")
+
+				if("loading" in string):
+					print("loading")
+				elif("delivery" in string):
+					print("delivery")
+				elif("discharge" in string):
+					print("discharge")
 				
 
 
 
+			
+	
 			if ('demurrage' in line.lower()):
+				print("--------demurrage----------")
 				dem_list =[]
 				dem = line.partition(':')
 				demu = str(dem[2]).replace(' ',"")
@@ -93,8 +217,9 @@ def main():
 				#print(dem_list)
 				if ('charterparty' in dem_list) or('cp' in dem_list) or ('c/p' in dem_list):
 					print("charter party")
-
+			
 			if ('laytime' in line.lower()):
+				print("--------laytime----------")
 				lay = line.partition(':')
 				layt = str(lay[2])
 				numbers = re.findall('\d+',layt)
